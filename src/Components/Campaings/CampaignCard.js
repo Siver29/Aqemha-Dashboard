acb5197e3678568aae3b1576dad19072feab1e30
Siver29 <mohammad111khayat@gmail.com>
2024-08-19 04:59:20 +0300

    card v.0

--- src/Components/Campaings/CampaignCard.js
+++ src/Components/Campaings/CampaignCard.js
@@ -1,4 +1,5 @@
 import React,{useState} from 'react'
+import '../Style/Style.css'
 
 function CampaignCard({ campaigns, showCampaign, selectCampaign,showFundCampaign ,handleToggleRefresh,type }) {
   console.log(campaigns.id)
@@ -34,45 +35,55 @@ function CampaignCard({ campaigns, showCampaign, selectCampaign,showFundCampaign
     setConfirmDelete(false);
   };
   return (
-    <div className='card mb-4'>
+    <div className='card mb-4 campaignCard'>
       {type == 'camp' && (
-        <div>
-          <button onClick={() => showCampaign(campaigns.id)}>edit</button>
-          <div onClick={() => selectCampaign(campaigns.id)}>
-            <h3>{campaigns.name}</h3>
+        <div className='text-start'>
+          <button className='btn ' onClick={() => showCampaign(campaigns.id)}><img src='/imges/edit-button-svgrepo-com.svg' className='img-fluid' width={25} height={25}/></button>
+          <div className='text-center campName' onClick={() => selectCampaign(campaigns.id)}>
+            <h2>{campaigns.name}</h2>
           </div>
         </div>
       )}
       {type == 'fund' && (
-        <div>
-          <button onClick={() => showFundCampaign(campaigns.id)}>edit111</button>
-          <button onClick={() => deleteFundCampaign(campaigns.id)}>delete</button>
-            <h3>{campaigns.name}</h3>
+        <div className='text-start mb-3'>
+          <button className='btn ' onClick={() => showFundCampaign(campaigns.id)}><img src='/imges/edit-button-svgrepo-com.svg' className='img-fluid' width={25} height={25}/></button>
+          <button className='btn ' onClick={() => deleteFundCampaign(campaigns.id)}><img src='/imges/delete-svgrepo-com.svg' className='img-fluid' width={25} height={25}/></button>
+            <h2 className='text-center mt-4'>{campaigns.name}</h2>
         </div>
       )}
       {campaigns.photo && (
-        <img src={`http://localhost:8000/${campaigns.photo}`} />
+        <img src={`http://localhost:8000${campaigns.photo}`} className='mb-3 w-50 mx-auto' />
       )}
+      {campaigns.current_percentage && (
+        <div 
+        className='mx-auto mb-3'
+        role="progressbar" 
+        aria-valuenow={campaigns.current_percentage} 
+        aria-valuemin="0" 
+        aria-valuemax="100" 
+        style={{ "--value": campaigns.current_percentage }}
+      ></div>        // <p>{campaigns.current_percentage}</p>
+      )}
+        {campaigns.caption && (
+          <p> {campaigns.caption}</p>
+        )}
+        {campaigns.start_date && (
+          <p>تاريخ بداية الحملة :  {campaigns.start_date}</p>
+        )}
       {campaigns.end_date && (
-        <p>{campaigns.current_percentage}</p>
-      )}
-      {campaigns.end_date && (
-        <p>{campaigns.end_date}</p>
-      )}
-      {campaigns.start_date && (
-        <p>{campaigns.start_date}</p>
+        <p>تاريخ نهاية الحملة :{campaigns.end_date}</p>
       )}
       {campaigns.volunteer_needed && (
-        <span>{campaigns.volunteer_needed}</span>
+        <span>عدد المتطوعين المطلوب :{campaigns.volunteer_needed}</span>
       )}
       {campaigns.volunteer_number && (
-        <span>{campaigns.volunteer_number}</span>
+        <span>عدد المطوعين المشاركين : {campaigns.volunteer_number}</span>
       )}
       {confirmDelete && (
-        <div>
-          <p>Are you sure you want to delete this campaign?</p>
-          <button onClick={() => handleConfirmDelete(campaigns.id)}>Yes, delete</button>
-          <button onClick={handleCancelDelete}>Cancel</button>
+        <div className='mb-3'>
+          <p>هل أنت متأكد من حذف الحملة؟</p>
+          <button onClick={() => handleConfirmDelete(campaigns.id)} className='btn btn-danger mx-3'>نعم</button>
+          <button onClick={handleCancelDelete} className='btn btn-primary'>لا</button>
         </div>
       )}
     </div>
